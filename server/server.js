97aa8527529b11dce926899e8219c5e737e90e36
Sangeet Kumar <135730503+Sangeet96@users.noreply.github.com>
2024-07-11 12:54:41 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { MongoClient } = require('mongodb');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+dotenv.config()
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'cyberkey';
+
+// console.log(process.env.MONGO_URI)
+
+const app = express()
+const port = 3000
+app.use(bodyparser.json());
+app.use(cors());
+
+client.connect();
+console.log('Connected successfully to server');
+
+// Get all the passwords
+app.get('/', async (req, res) => {
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.find({}).toArray();
+    res.json(findResult)
+    // res.send('hello')
+})
+
+// save passwords
+app.post('/', async (req, res) => {
+    const password = req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.insertOne(password);
+    res.send({success:true, result: findResult})
+    // res.send('hello')
+})
+
+// delete passwords
+app.delete('/', async (req, res) => {
+    const password = req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.deleteOne(password);
+    res.send({success:true, result: findResult})
+    // res.send('hello')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
