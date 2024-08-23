035f5f602ba133d0ce007c24e021da661042cd15
Sangeet Kumar <135730503+Sangeet96@users.noreply.github.com>
2024-08-23 14:29:52 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Body from './components/Body'
-import axios from 'axios'
 
 function App() {
-  axios.defaults.withCredentials = true;
   return (
     <>
       <Navbar/>
